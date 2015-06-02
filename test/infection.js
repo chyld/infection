465f7719237755132b2d18929bfc7a8c503c5189
Chyld Medford <chyld.medford@gmail.com>
2015-06-02 02:26:46 -0700

    limitedInfection

--- test/infection.js
+++ test/infection.js
@@ -33,3 +33,31 @@ QUnit.test(".totalInfection", function(assert){
 
   assert.equal(infected, 9, "should infect 9 connected users");
 });
+
+QUnit.test(".limitedInfection", function(assert){
+  users[0].addStudent(users[1]);
+  users[0].addStudent(users[2]);
+
+  users[2].addStudent(users[3]);
+  users[2].addStudent(users[4]);
+  users[2].addStudent(users[5]);
+
+  users[4].addStudent(users[0]);
+  users[4].addStudent(users[1]);
+
+  users[5].addStudent(users[2]);
+  users[5].addStudent(users[6]);
+  users[5].addStudent(users[8]);
+  users[5].addStudent(users[10]);
+
+  users[8].addStudent(users[0]);
+  users[8].addStudent(users[5]);
+
+  Infection.limitedInfection(users, users[0], 2, 4);
+
+  var infected = users.reduce(function(sum, user){
+    return sum += user.version === 2 ? 1 : 0;
+  }, 0);
+
+  assert.equal(infected, 6, "should only infect 6 users");
+});
