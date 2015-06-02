55f76fe29210b2d239695d4f4460ec3928709f12
Chyld Medford <chyld.medford@gmail.com>
2015-06-02 01:09:12 -0700

    add student

--- test/factory.js
+++ test/factory.js
@@ -2,34 +2,48 @@ function Factory(){
 }
 
 Factory.users = function(){
-  var a = new User('a', 1);
-  var b = new User('b', 1);
-  var c = new User('c', 1);
-  var d = new User('d', 1);
-  var e = new User('e', 1);
-  var f = new User('f', 1);
-  var g = new User('g', 1);
-  var h = new User('h', 1);
-  var i = new User('i', 1);
-  var j = new User('j', 1);
-  var k = new User('k', 1);
-  var l = new User('l', 1);
-  var m = new User('m', 1);
-  var n = new User('n', 1);
-  var o = new User('o', 1);
-  var p = new User('p', 1);
-  var q = new User('q', 1);
-  var r = new User('r', 1);
-  var s = new User('s', 1);
-  var t = new User('t', 1);
-  var u = new User('u', 1);
-  var v = new User('v', 1);
-  var w = new User('w', 1);
-  var x = new User('x', 1);
-  var y = new User('y', 1);
-  var z = new User('z', 1);
+  var a = new User("A", 1);
+  var b = new User("B", 1);
+  var c = new User("C", 1);
+  var d = new User("D", 1);
+  var e = new User("E", 1);
+  var f = new User("F", 1);
+  var g = new User("G", 1);
+  var h = new User("H", 1);
+  var i = new User("I", 1);
+  var j = new User("J", 1);
+  var k = new User("K", 1);
+  var l = new User("L", 1);
+  var m = new User("M", 1);
+  var n = new User("N", 1);
+  var o = new User("O", 1);
+  var p = new User("P", 1);
+  var q = new User("Q", 1);
+  var r = new User("R", 1);
+  var s = new User("S", 1);
+  var t = new User("T", 1);
+  var u = new User("U", 1);
+  var v = new User("V", 1);
+  var w = new User("W", 1);
+  var x = new User("X", 1);
+  var y = new User("Y", 1);
+  var z = new User("Z", 1);
 
   var users = [];
   users.push(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
+  return users;
+};
+
+Factory.relationships = function(rounds){
+  var users = Factory.users();
+
+  for(var i = 0; i < rounds; i++){
+    var a = Math.floor(Math.random() * 26);
+    var b = Math.floor(Math.random() * 26);
+    if(a !== b){
+      users[a].addStudent(users[b]);
+    }
+  }
+
   return users;
 };
--- test/user.js
+++ test/user.js
@@ -10,8 +10,19 @@ QUnit.test("new", function(assert){
   assert.ok(users[0] instanceof User, "should be a User");
   assert.ok(users[0].id, "should have a user id");
   assert.ok(users[0].createdAt, "should have a createdAt time");
-  assert.equal(users[0].name, "a", "should have name a");
+  assert.equal(users[0].name, "A", "should have name A");
   assert.equal(users[0].version, 1, "should be version 1");
   assert.equal(users[0].teachers.length, 0, "should have no teachers");
   assert.equal(users[0].students.length, 0, "should have no students");
+});
+
+QUnit.test("#addStudent", function(assert){
+  users[0].addStudent(users[1]);
+  users[0].addStudent(users[1]);
+  users[0].addStudent(users[1]);
+  users[0].addStudent(users[2]);
+  assert.equal(users[0].students.length, 2, "should have 2 students");
+  assert.equal(users[0].teachers.length, 0, "should have no teachers");
+  assert.equal(users[1].teachers.length, 1, "should have 1 teacher");
+  assert.equal(users[2].teachers.length, 1, "should have 2 teachers");
 });
